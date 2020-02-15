54453a34f05fb9b4a0285e5de85000d7c0fa259c
Sharofiddin Azizmatov <napster1202@gmail.com>
2020-02-15 09:11:17 +0200

    init vue/vuetify/vuex

---
+++ client/src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import vuetify from "@/plugins/vuetify";
+import { sync } from "vuex-router-sync";
+
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+sync(store, router);
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
